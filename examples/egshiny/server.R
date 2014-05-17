b48c5326cd01813375946c85c592a03e578868e8
stephens999 <stephens999@gmail.com>
2014-05-16 22:36:38 -0500

    added shiny example

---
+++ examples/egshiny/server.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+#plot a histogram of z scores, highlighting the alternative distribution
+#of z scores that is implied by pi0
+nullalthist_z = function(z,pi0,nullcol="blue",altcol="cyan",...){
+  h=hist(z, freq=FALSE,col=nullcol,nclass=ncz,ylim=c(-0.5,0.5),...)
+  nb = length(h$breaks)
+  nullexp = pi0 * (pnorm(h$breaks)[-1] - pnorm(h$breaks[-nb]))/(h$breaks[-1]-h$breaks[-nb])
+  h$density = h$density - nullexp
+  plot(h,add=TRUE,col=altcol,freq=FALSE)
+}
+set.seed(100)
+z = c(rnorm(5000,0,2),rnorm(5000,0,1))
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  output$distPlot <- renderPlot({
+    # draw the histogram with the specified number of bins
+    nullalthist_z(z, input$pi0)
+  })
+})
---
+++ examples/egshiny/ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Interactive Unimodal Demo"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("pi0",
+                  "pi0:",
+                  min = 0,
+                  max = 1,
+                  value = 0),
+      
+      helpText("The histogram shows a distribution of z scores, some null (blue) and some non-null (cyan).", 
+               "You control what proportion are colored as being from the null by changing pi0.",
+    "The remainder are implicitly colored as being non-null.",
+    "As you increase pi0, the distribution of the non-null z becomes less unimodal.",
+"ash tries to find the largest pi0 that leaves the cyan distribution unimodal.",
+"qvalue tries to find the largest pi0 that leaves the cyan distribution non-negative.")
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
