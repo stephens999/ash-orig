bd7ab1d39e7171ee5520ab8c2e02521a20cd695c
stephens999 <stephens999@gmail.com>
2015-04-14 11:27:12 -0500

    changed dsc-shrink to use cxx

--- dsc-shrink/addMethods.R
+++ dsc-shrink/addMethods.R
@@ -1,18 +1,18 @@
 sourceDir("methods")
 
-addMethod(dsc_shrink,name="ash.hu.cxx",fn =ash.wrapper,args=list(mixcompdist="halfunif",cxx=TRUE),outputtype = "ash_output")
-addMethod(dsc_shrink,name="ash.hu",fn =ash.wrapper,args=list(mixcompdist="halfunif"),outputtype = "ash_output")
-addMethod(dsc_shrink,name="ash.u",fn =ash.wrapper,args=list(mixcompdist="unif"),outputtype = "ash_output")
-addMethod(dsc_shrink,name="ash.n",fn =ash.wrapper,args=list(mixcompdist="normal"),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.hu.nocxx",fn =ash.wrapper,args=list(mixcompdist="halfunif",cxx=FALSE),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.hu",fn =ash.wrapper,args=list(mixcompdist="halfunif",cxx=TRUE),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.u",fn =ash.wrapper,args=list(mixcompdist="unif",cxx=TRUE),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.n",fn =ash.wrapper,args=list(mixcompdist="normal",cxx=TRUE),outputtype = "ash_output")
 
 addMethod(dsc_shrink,name="ash.hu.nw2",fn =ash.wrapper,args=list(mixcompdist="halfunif",nullweight=2,cxx=TRUE),outputtype = "ash_output")
 addMethod(dsc_shrink,name="ash.u.nw2",fn =ash.wrapper,args=list(mixcompdist="unif",nullweight=2,cxx=TRUE),outputtype = "ash_output")
 addMethod(dsc_shrink,name="ash.n.nw2",fn =ash.wrapper,args=list(mixcompdist="normal",nullweight=2,cxx=TRUE),outputtype = "ash_output")
 
 
-addMethod(dsc_shrink,name="ash.hu.s",fn =ash.wrapper,args=list(mixcompdist="halfunif",method="shrink"),outputtype = "ash_output")
-addMethod(dsc_shrink,name="ash.u.s",fn =ash.wrapper,args=list(mixcompdist="unif",method="shrink"),outputtype = "ash_output")
-addMethod(dsc_shrink,name="ash.n.s",fn =ash.wrapper,args=list(mixcompdist="normal",method="shrink"),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.hu.s",fn =ash.wrapper,args=list(mixcompdist="halfunif",method="shrink",cxx=TRUE),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.u.s",fn =ash.wrapper,args=list(mixcompdist="unif",method="shrink",cxx=TRUE),outputtype = "ash_output")
+addMethod(dsc_shrink,name="ash.n.s",fn =ash.wrapper,args=list(mixcompdist="normal",method="shrink",cxx=TRUE),outputtype = "ash_output")
 
 
 addMethod(dsc_shrink,name="mixfdr.tnull", fn=mixfdr.wrapper, args = list(theonull=TRUE),outputtype = "mixfdr_output")
