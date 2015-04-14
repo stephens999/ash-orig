968fbb9063da95cdeafc4b1d8641cfc9620fa93d
stephens999 <stephens999@gmail.com>
2015-04-14 11:55:36 -0500

    fixing dsc-robust

---
+++ dsc-robust/run_dsc_robust.R
@@ -0,0 +1,42 @@
+library(dscr)
+sourceDir("methods")
+
+###### Initialize #######
+
+dsc_robust=new.dsc("robust","dsc-robust-files")
+
+###### Add Scenarios #####
+sourceDir("../dsc-shrink/datamakers")
+source("../dsc-shrink/addNamedScenarios.R")
+addNamedScenarios(dsc_robust,c("spiky","near-normal","flat-top","skew","big-normal","bimodal"))
+
+
+###### Add Methods #####
+
+addMethod(dsc_robust,"ash.u",ash.wrapper,args=list(mixcompdist="uniform",cxx=TRUE))
+addMethod(dsc_robust,"ash.hu",ash.wrapper,args=list(mixcompdist="halfuniform",cxx=TRUE))
+addMethod(dsc_robust,"ash.n",ash.wrapper,args=list(mixcompdist="normal",cxx=TRUE))
+
+
+
+####### Define Score and Add it #######
+
+score = function(data, output){
+  x=output$loglik
+  #names(x)=paste0('C',1:length(x))
+  #class(x)<-'data.frame'
+  return(list(diff1 = max(x)-x[1],diff2=(max(x)-min(x))))
+}
+
+addScore(dsc_robust,score)
+
+######## Run the DSC #################
+
+#reset_dsc(dsc_robust,force=TRUE)
+res_robust=run_dsc(dsc_robust)
+save(res_robust,file="res_robust.RData")
+
+
+
+
+
