ba5289af8067477abd469c1075b1d778c27e2b15
stephens999 <stephens999@gmail.com>
2015-04-14 11:11:47 -0500

    started editing addScenarios

--- dsc-shrink/addScenarios.R
+++ dsc-shrink/addScenarios.R
@@ -1,132 +1,114 @@
 sourceDir("datamakers")
 
 
-#Now, for each scenario create an element of scenarios of the following form
-addScenario(dsc_shrink,name="A",
+addNamedScenarios = function(dsc,names,nsamp=1000){
+
+#the -nn suffix indicates no nulls (min_pi0 = max_pi0 = 0)
+
+if("near-normal-nn" %in% names){
+	addScenario(dsc,name="near-normal-nn",
                     fn=rnormmix_datamaker,
                     args=list(
                       g=normalmix(c(2/3,1/3),c(0,0),c(1,2)),
                       min_pi0=0,
                       max_pi0=0,
-                      nsamp=1000,
+                      nsamp=nsamp,
                       betahatsd=1
                       ),
                     seed=1:100)
+}
 
-addScenario(dsc_shrink,name="B",
+if("flat-top-nn" %in% names){
+	addScenario(dsc,name="flat-top-nn",
                     fn=rnormmix_datamaker,
                     args=list(
                       g=normalmix(rep(1/7,7),c(-1.5,-1,-0.5,0,0.5,1,1.5),rep(0.5,7)),
                       min_pi0=0,
                       max_pi0=0,
-                      nsamp=1000,
+                      nsamp=nsamp,
                       betahatsd=1
                     ),
                     seed=1:100)
+}
 
-
-addScenario(dsc_shrink,name="C",
+addScenario(dsc,name="skew-nn",
                       fn=rnormmix_datamaker,
                       args=list(
                         g=normalmix(c(1/4,1/4,1/3,1/6),c(-2,-1,0,1),c(2,1.5,1,1)),
                         min_pi0=0,
                         max_pi0=0,
-                        nsamp=1000,
+                        nsamp=nsamp,
                         betahatsd=1
                       ),
                       seed=1:100)
 
-#scenarios An, Bn, Cn are the same as A,B,C but with nulls included (pi0 uniform on [0,1])
-addScenario(dsc_shrink,name="An",
+addScenario(dsc,name="near-normal",
                     fn=rnormmix_datamaker,
                     args=list(
                       g=normalmix(c(2/3,1/3),c(0,0),c(1,2)),
                       min_pi0=0,
                       max_pi0=1,
-                      nsamp=1000,
+                      nsamp=nsamp,
                       betahatsd=1
                     ),
                     seed=1:100)
 
-addScenario(dsc_shrink,name="Bn",
+addScenario(dsc,name="flat-top",
                     fn=rnormmix_datamaker,
                     args=list(
                       g=normalmix(rep(1/7,7),c(-1.5,-1,-0.5,0,0.5,1,1.5),rep(0.5,7)),
                       min_pi0=0,
                       max_pi0=1,
-                      nsamp=1000,
+                      nsamp=nsamp,
                       betahatsd=1
                     ),
                     seed=1:100)
 
 
-addScenario(dsc_shrink,name="Cn",
+addScenario(dsc,name="skew",
                       fn=rnormmix_datamaker,
                       args=list(
                         g=normalmix(c(1/4,1/4,1/3,1/6),c(-2,-1,0,1),c(2,1.5,1,1)),
                         min_pi0=0,
                         max_pi0=1,
-                        nsamp=1000,
+                        nsamp=nsamp,
                         betahatsd=1
                       ),
                       seed=1:100)
 
-addScenario(dsc_shrink,name="hard",
+addScenario(dsc,name="spiky",
                       fn=rnormmix_datamaker,
                       args=list(
                         g=normalmix(c(.4,.2,.2,.2),c(0,0,0,0),c(.25,.5,1,2)),
                         min_pi0=0,
                         max_pi0=1,
-                        nsamp=1000,
+                        nsamp=nsamp,
                         betahatsd=1
                       ),
                       seed=1:100)
 
-addScenario(dsc_shrink,name="easy",
+addScenario(dsc,name="big-normal",
                       fn=rnormmix_datamaker,
                       args=list(
                         g=normalmix(c(1),c(0),c(4)),
                         min_pi0=0,
                         max_pi0=1,
-                        nsamp=1000,
+                        nsamp=nsamp,
                         betahatsd=1
                       ),
                       seed=1:100)
 
-addScenario(dsc_shrink,name="efron_FCR",
-            fn=rnormmix_datamaker,
-            args=list(
-              g=normalmix(c(1),c(-3),c(1)),
-              min_pi0=0.9,
-              max_pi0=0.9,
-              nsamp=1000,
-              betahatsd=1
-            ),
-            seed=1:100)
 
-
-addScenario(dsc_shrink,name="bimodal",
+addScenario(dsc,name="bimodal",
             fn=rnormmix_datamaker,
             args=list(
               g=normalmix(c(0.5,0.5),c(-2,2),c(1,1)),
               min_pi0=0,
               max_pi0=1,
-              nsamp=1000,
+              nsamp=nsamp,
               betahatsd=1
             ),
             seed=1:100)
 
 
-
-
-# addScenario(dsc_shrink,name="hard-b",
-#                       fn=rnormmix_datamaker,
-#                       args=list(
-#                         g=normalmix(c(.4,.2,.2,.2),c(0,0,0,0),c(.25,.5,1,2)),
-#                         min_pi0=0,
-#                         max_pi0=1,
-#                         nsamp=10000,
-#                         betahatsd=1
-#                       ),
-#                       seed=1:100)
-
+}
